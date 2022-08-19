5885591b1c7807b5b67f1eb503b666ff7ef4f126
Pragnesh Amadavadi <pragnesh.amadavadi07@gmail.com>
2022-08-19 13:56:49 +0530

    passed 8 test case

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,12 @@
-var Pragnen_19IT002 = artifacts.require("./Pragnen_19IT002.sol");
+const Pragnen_19IT002 = artifacts.require("./Pragnen_19IT002.sol");
+const PragnenTokenSale = artifacts.require("./PragnenTokenSale.sol");
+const tokenPrice = 1000000000000000;
 
 module.exports = function(deployer) {
-  deployer.deploy(Pragnen_19IT002, 1000000);
-  
-//   .then(function() {
-//     // Token price is 0.001 Ether
-//     var tokenPrice = 1000000000000000;
-//     return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
-//   });
+  deployer.deploy(Pragnen_19IT002,1000000).then(()=>{
+    return deployer.deploy(PragnenTokenSale,Pragnen_19IT002.address,tokenPrice);
+  });
 };
+
+
+
